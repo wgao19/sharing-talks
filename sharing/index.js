6eb582eff988df5b65a7e31c513aa66d011fbeeb
Wei Gao <wgao19@gmail.com>
2018-08-26 11:50:55 +0800

    wrap up

---
+++ sharing/index.js
@@ -0,0 +1,7 @@
+export { yellow as theme } from "mdx-deck/themes";
+import cover from "./cover.mdx";
+import motivation from "./motivation.mdx";
+import slc from "./slc.mdx";
+import rr from "./reactrally";
+
+export default [...cover, ...motivation, ...slc, ...rr];
---
+++ sharing/reactrally/index.js
@@ -0,0 +1,27 @@
+import cover from "./cover.mdx";
+import sunilpai from "./sunilpai.mdx";
+import socialimpact from "./socialimpact.mdx";
+import henryzhu from "./henryzhu.mdx";
+import shawnwang from "./shawnwang.mdx";
+import michaelchan from "./michaelchan.mdx";
+import brianholt from "./brianholt.mdx";
+import joshuacomeau from "./joshuacomeau.mdx";
+import jenn from "./jenn.mdx";
+import ryanmore from "./ryanmore.mdx";
+import listoftalks from "./listoftalks.mdx";
+import thereactrallyway from "./thereactrallyway.mdx";
+
+export default [
+  ...cover,
+  ...sunilpai,
+  ...socialimpact,
+  ...ryanmore,
+  ...henryzhu,
+  ...shawnwang, // why react is not reactive
+  ...brianholt, // chaos monkey
+  ...michaelchan, // clean code is dead
+  ...jenn,
+  ...joshuacomeau, // explorable explanations
+  ...listoftalks,
+  ...thereactrallyway
+];
